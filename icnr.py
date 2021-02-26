9f646cf47bdf4fca7450ba5f16866d062116236b
Steven Bamford <steven@stevenbamford.com>
2021-02-26 22:22:37 +0000

    Fix implementation
    I may be missing something, but the implementation didn't seem right.
    The output size didn't always match that requested, and there was some
    odd mixing of the filter and spatial axes. I've now altered it so now
    the spatial axes are simply 'block replicated' in what seems like a
    more intuitive and correct manner.

--- icnr.py
+++ icnr.py
@@ -1,35 +1,35 @@
-from __future__ import division
-
+import numpy as np
 import tensorflow as tf
-
+from tensorflow.python.layers.utils import normalize_tuple
 
 class ICNR:
-    """ICNR initializer for checkerboard artifact free sub pixel convolution
-
-    Ref:
-     [1] Andrew Aitken et al. Checkerboard artifact free sub-pixel convolution
-     https://arxiv.org/pdf/1707.02937.pdf)
-
-    Args:
-    initializer: initializer used for sub kernels (orthogonal, glorot uniform, etc.)
-    scale: scale factor of sub pixel convolution
-    """
-
     def __init__(self, initializer, scale=1):
-        self.scale = scale
+        """ICNR initializer for checkerboard artifact free transpose convolution
+
+        Code adapted from https://github.com/kostyaev/ICNR
+        Discussed at https://github.com/Lasagne/Lasagne/issues/862
+        Original paper: https://arxiv.org/pdf/1707.02937.pdf
+
+        Parameters
+        ----------
+        initializer : Initializer
+            Initializer used for kernels (glorot uniform, etc.)
+        scale : iterable of two integers, or a single integer
+            Stride of the transpose convolution
+            (a.k.a. scale factor of sub pixel convolution)
+        """
+        self.scale = normalize_tuple(scale, 2, "scale")
         self.initializer = initializer
 
     def __call__(self, shape, dtype):
-        shape = list(shape)
         if self.scale == 1:
             return self.initializer(shape)
-
-        new_shape = shape[:3] + [shape[3] // (self.scale ** 2)]
+        size = shape[:2]
+        new_shape = np.array(shape)
+        new_shape[:2] //= self.scale
         x = self.initializer(new_shape, dtype)
         x = tf.transpose(x, perm=[2, 0, 1, 3])
-        size = (shape[0] * self.scale, shape[1] * self.scale)
         x = tf.image.resize(x, size=size, method="nearest")
-        x = tf.nn.space_to_depth(x, block_size=self.scale)
         x = tf.transpose(x, perm=[1, 2, 0, 3])
-
         return x
+
